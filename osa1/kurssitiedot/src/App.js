d3dc1a4528ad3951097f94d83b8cf2d6275b6c34
kaimhall <kaimhall@gmail.com>
2022-02-01 16:50:56 +0200

    update  1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {props.course.name}
+      </p>
     </div>
+  )
+}
+
+const Part = (props) => {
+  //console.log(props)
+  return (
+    <div>
+      <p>{props.name} {props.exer}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+        <Part name= {props.course.parts[0].name} exer={props.course.parts[0].exercises} />
+        <Part name= {props.course.parts[1].name} exer={props.course.parts[1].exercises} />
+        <Part name= {props.course.parts[2].name} exer={props.course.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log('Here !!')
+  return (
+    <div>
+      <p>
+        Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} 
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+    {
+      name:'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to passdata',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+  return (
+    <>
+     <h1><Header course= {course} /></h1>
+     <Content course={course} />
+     <Total course= {course} />
+    </>
   );
 }
 
