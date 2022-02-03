3738360e696c298891a0a7659c7f52858c7a7603
kaimhall <kaimhall@gmail.com>
2022-02-03 11:40:49 +0200

    end of part1

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,50 +1,24 @@
 import React, {useState} from 'react';
 
-const History = (props) => {
-  if (props.allClicks.length ===0) {
-    return(
-      <div>
-        the app is used by pressing the buttons
-        </div>
-      )
-  }
-  return(
-    <div>
-      button press history: {props.allClicks.join('')}
-    </div>
+const Display = props => <div>{props.val}</div>
+
+const Button = (props) => (
+  <button onClick= {props.handleClick}>{props.text}</button>
   )
-}
 
-const Button = ({handleClick, text}) => {
-  return(
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )}
+const App = (props) => {
+  const [val, setVal] = useState(10)
 
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left +1)
-  }
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right +1)
-  }
+  const setToVal = (newVal) => () => {
+      setVal(newVal)
+    }
 
   return(
     <div>
-      <div>
-        { left }
-        <Button handleClick= {handleLeftClick} text= 'left'/>
-        <Button handleClick= {handleRightClick} text= 'right'/>
-        { right }
-        <History allClicks={allClicks} />
-      </div>
+      <Display val={val} />
+      <Button handleClick = {setToVal(1000)} text = 'thousand' />
+      <Button handleClick = {setToVal(0)} text = 'reset' />
+      <Button handleClick = {setToVal(val+1)} text = 'increment' />
     </div>
   )
 }
