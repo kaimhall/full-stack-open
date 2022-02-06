8128af8de1d15c3c72142d9b4dd86ae42cc52c6a
kaimhall <kaimhall@gmail.com>
2022-02-06 14:06:16 +0200

    committed unicafe 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,73 +1,61 @@
 import React, {useState} from 'react'
 
-const Statistics = (props) => {
-  if(props.val === 0) {
+const StatisticsLine = (props) => (
+  <table>
+    <tbody>
+      <tr>
+        <td>{props.text} {props.value}</td>
+      </tr>
+    </tbody>
+  </table>
+) 
+
+const Statistics = ({good, bad, neutral}) => {
+  const cnt = good + bad + neutral
+  if (cnt === 0) {  
     return (
       <div>No feedback given</div>
     )
   }
-  if(props.text === 'positive') {
+  else {
+    const average = ((good - bad )/cnt).toFixed(1)
+    const positive = ((good / cnt)*100).toFixed(1)
+  
     return (
-      <div>{props.text} {props.val} %</div>
-    )
+      <div>
+        <StatisticsLine text= 'good' value= {good} />
+        <StatisticsLine text= 'neutral' value= {neutral} />
+        <StatisticsLine text= 'bad' value= {bad} />
+        <StatisticsLine text= 'all' value= {cnt} />
+        <StatisticsLine text= 'average' value= {average} />
+        <StatisticsLine text= 'positive' value= {positive + '%'} />
+      </div>
+    )   
   }
-  return (
-   <div>{props.text} {props.val}</div>
-  )
 }
+
 const Button = (props) => (
   <button onClick= {props.handleClick}>
     {props.text}
   </button>
   )
-  
-const avg= ({good, bad, neutral}) => {
-  let cnt = (good + bad + neutral)
-  let res = (good - bad + (neutral * 0))
-  let a = res/cnt
-  if (isNaN(a)){
-    return(0)
-  }
-  return (a)
-}
-
-const pos = ({good , bad, neutral}) => {
-  let res = (good/(bad + neutral + good)*100).toFixed(13)
-  if (isNaN(res)){
-    return(0)
-  }
-  return(res)
-}
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setNBad] = useState(0)
-  
-  const obj = {
-    good: good,
-    bad: bad,
-    neutral:neutral
-  }
-
-  let average = avg(obj)
-  let positive = pos(obj)
-
-
-  console.log(good)
+  const [feedback, setFeedback] = useState(
+    {
+      good: 0,
+      bad: 0,
+      neutral: 0
+    } 
+  )
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good +1)} text= 'good' />
-      <Button handleClick={() => setNeutral(neutral +1)} text= 'neutral' />
-      <Button handleClick={() => setNBad(bad +1)} text= 'bad' />
+      <Button handleClick={() => setFeedback({...feedback, good: feedback.good +1})} text= 'good' />
+      <Button handleClick={() => setFeedback({...feedback, neutral: feedback.neutral +1})} text= 'neutral' />
+      <Button handleClick={() => setFeedback({...feedback, bad: feedback.bad +1})} text= 'bad' />
       <h1>statistics</h1>
-      <Statistics val= {good} text='good'/>
-      <Statistics val= {neutral} text='neutral'/>
-      <Statistics val= {bad} text='bad'/>
-      <Statistics val= {good+bad+neutral} text='all'/>
-      <Statistics val= {average} text='avegage'/>
-      <Statistics val= {positive} text='positive'/>
+      <Statistics {...feedback} />
     </div>
   );
 }
