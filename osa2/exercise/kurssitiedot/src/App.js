cd1665cc4c02f53dfe5c95dce2bbf379b3d9669d
kaimhall <kaimhall@gmail.com>
2022-02-15 14:31:46 +0200

    ex 2.5 kurssitiedot 2.5

--- osa2/exercise/kurssitiedot/src/App.js
+++ osa2/exercise/kurssitiedot/src/App.js
@@ -1,40 +1,4 @@
-
-const Course = ({course}) => {
-  const courses = course.map( c => c)
-  
-  const Header = ({course}) => <h3 key={course.id}> {course.name} </h3>
-  const Parts = ({course}) => course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>) 
-  const Content = ({course}) => <Parts course={course} />
-  
-  const Total= ({course}) => {
-    const e = course.parts.map(part => part.exercises)
-    return (
-      <div>
-        <b>
-          total of {e.reduce((accumulator, curr) => accumulator + curr)} exercises
-        </b>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h1> Web development curriculum </h1>
-      {
-        courses.map (
-          course => {
-            return (
-              <div key={course.id}>
-                <Header course= {course} />
-                <Content course = {course} />
-                <Total course = {course} />
-              </div>
-            )
-          }
-        )
-      }
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -96,7 +60,5 @@ const App = () => {
     <Course course={courses} />
   )
 }
-
-
  
 export default App
---
+++ osa2/exercise/kurssitiedot/src/components/Course.js
@@ -0,0 +1,39 @@
+const Course = ({course}) => {
+    const courses = course.map( c => c)
+    
+    const Header = ({course}) => <h3 key={course.id}> {course.name} </h3>
+    const Parts = ({course}) => course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>) 
+    const Content = ({course}) => <Parts course={course} />
+    
+    const Total= ({course}) => {
+      const e = course.parts.map(part => part.exercises)
+      return (
+        <div>
+          <b>
+            total of {e.reduce((accumulator, curr) => accumulator + curr)} exercises
+          </b>
+        </div>
+      )
+    }
+
+    return (
+        <div>
+            <h1> Web development curriculum </h1>
+            {
+                courses.map (
+                    course => {
+                        return (
+                            <div key={course.id}>
+                            <Header course= {course} />
+                            <Content course = {course} />
+                            <Total course = {course} />
+                            </div>
+                        )
+                    }
+                )
+            }
+        </div>
+    )
+  }
+
+  export default Course
