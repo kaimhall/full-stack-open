ce380b1147eece522f3fcaf1bc8a98341460f0f8
kaimhall <kaimhall@gmail.com>
2022-02-17 18:32:44 +0200

    2.11 puhelinluettelo step 6

--- osa2/exercise/puhelinluettelo/src/App.js
+++ osa2/exercise/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({changeHandler}) => {
   return (
@@ -50,13 +51,14 @@ const Persons = ({persons, filterName}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data)
+    )},
+    [])
+  console.log(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
