513ab10832ce01bc07b8853daf7122e4bee73e45
kaimhall <kaimhall@gmail.com>
2022-02-17 07:07:35 +0200

    exercise 2.6

--- osa2/exercise/puhelinluettelo/src/App.js
+++ osa2/exercise/puhelinluettelo/src/App.js
@@ -5,23 +5,42 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const changeHandler = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit = {addPerson}>
         <div>
-          name: <input />
+          name:  
+          <input
+            value = {newName}
+            onChange = {changeHandler}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {
+      persons.map(p => <div key={p.name}>{p.name}</div>)
+      }
     </div>
   )
-
 }
 
 export default App
