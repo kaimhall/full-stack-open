c078c0bdc7205a581603037ae0ebc646a9736196
kaimhall <kaimhall@gmail.com>
2022-02-17 14:48:09 +0200

    train 2b

--- osa2/train/part2/src/index.js
+++ osa2/train/part2/src/index.js
@@ -1,32 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: false
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: true
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: false
-  }
-]
-//const result = notes.map(note => note.id)
-//console.log(result)
-
-ReactDOM.render(
-  <App notes= {notes} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'))
 
 
