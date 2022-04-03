baac4ac4fefd9f0bbb394192ad685398a1db9995
kaimhall <kaimhall@gmail.com>
2022-04-03 09:26:02 +0300

    state before 3.1

--- osa3/train/index.js
+++ osa3/train/index.js
@@ -1,5 +1,8 @@
+const  response = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -41,6 +44,41 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404).end()
   }
 })
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note =>  note.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error:'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  
+  notes = notes.concat(note)
+  res.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
