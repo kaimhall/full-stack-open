585575644dd816940a0197760998515bf7c61783
kaimhall <kaimhall@gmail.com>
2022-04-09 11:02:46 +0300

    3.21 train commit

--- osa3/train/index.js
+++ osa3/train/index.js
@@ -31,11 +31,9 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+  
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -44,6 +42,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -68,26 +67,29 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  const note = {
-    content:body.content,
-    important:body.important,
-  }
-  Note.findByIdAndUpdate(request.params.id, note, { new:true})
-  .then(updatedNote => {
-    response.json(updatedNote)
-  })
-  .catch(error => next(error))
+  const {content, important} = request.body 
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+    { new:true, runValidators:true, context: 'query'}
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint'})
-}
+} 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
