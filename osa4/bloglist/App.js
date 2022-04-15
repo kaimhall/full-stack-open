d2e45672f20b84f8da80533bf76b60a2dcccdf6c
kaimhall <kaimhall@gmail.com>
2022-04-15 22:08:01 +0300

    4.3

--- osa4/bloglist/App.js
+++ osa4/bloglist/App.js
@@ -18,9 +18,9 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB', error.message)
   })
 
-  app.use(cors())
-  app.use(express.json())
-  app.use(middleware.requestLogger)
-  app.use('/api/blogs', postRouter)
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', postRouter)
 
-  module.exports = app
+module.exports = app
