75bb2401f102ff0433aa4818839a26848bb5e324
kaimhall <kaimhall@gmail.com>
2022-05-04 12:02:45 +0300

    5.2 step2

--- osa5/bloglist/frontend/src/App.js
+++ osa5/bloglist/frontend/src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +16,88 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
+  useEffect(() => {
+    const logUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (logUserJSON) {
+      const user = JSON.parse(logUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+const loginHandler = async (event) => {
+  event.preventDefault()
+  try {
+    const user = await loginService.login({
+      username, password,
+    })
+    window.localStorage.setItem(
+      'loggedBloglistUser', JSON.stringify(user)
+    ) 
+    setUser(user)
+      setUsername('')
+      setPassword('') 
+  } catch (exception) {
+    setErrorMessage('wrong username or password')
+    setTimeout( () => {
+      setErrorMessage(null)
+    }, 2000)
+  }
+}
+
+const loginForm = () => (
+  <div>
+    <h2>Log in to application</h2>
+    <form onSubmit={loginHandler}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+            />
+        </div>
+        <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+)
+
+const logoutHandler = (event) => {
+  event.preventDefault()
+  window.localStorage.removeItem('loggedblogListUser')
+  setUser(null)
+}
+
+const blogForm = () => (
+  <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <form onSubmit = {logoutHandler} >
+        <p>{user.name} logged in
+          <button type="submit"> logout</button>
+        </p>
+      </form>
+
+      {blogs.map( blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+  </div>
+)
+
+  return (
+    <div>
+      {user === null ?
+      loginForm() :
+      blogForm()
+    }
     </div>
   )
 }
