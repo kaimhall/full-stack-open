dca052fc4e512c26b7494faf7c1ba7da5ab34a8e
kaimhall <kaimhall@gmail.com>
2022-05-10 12:02:44 +0300

    cypress up and running

--- osa5/bloglist/backend/App.js
+++ osa5/bloglist/backend/App.js
@@ -29,6 +29,12 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', userExtractor, postRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controller/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(errorHandler)
 
