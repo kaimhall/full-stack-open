120f10b65b578195529750909e0571f5d9eebf1d
kaimhall <kaimhall@gmail.com>
2022-05-25 08:06:40 +0300

    part 7 exercises

---
+++ osa7/bundle/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+    const notes = useNotes(BACKEND_URL)
+  }
+  const url = 'https://webpackertesting.herokuapp.com/api/notes'
+  const notes = useNotes(url)
+  
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}> press</button>
+      <div>{notes.length} notes on server {BACKEND_URL}</div>
+    </div>
+  )
+}
+
+export default App
